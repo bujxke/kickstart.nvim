b1834aa80385ad9a599dbb986c852c39d45f79bb
bujxke <buji@worldlibertyfinancial.com>
2026-01-20 15:42:32 +0200

    feat: remap jumplist keys

--- init.lua
+++ init.lua
@@ -178,6 +178,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<C-[>', '<C-o>', { desc = 'Jump back' })
+vim.keymap.set('n', '<C-]>', '<C-i>', { desc = 'Jump forward' })
+vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
